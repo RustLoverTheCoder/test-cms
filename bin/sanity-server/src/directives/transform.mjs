8b0bd47135f8aa6bb7d888ed8c7662c211faf3fd
ranbo <71162666+RustLoverTheCoder@users.noreply.github.com>
2024-06-25 18:52:30 +0800

    feat: add transformDirectiveTransformer

--- bin/sanity-server/src/directives/transform.mjs
+++ bin/sanity-server/src/directives/transform.mjs
@@ -3,7 +3,7 @@ import { getDirective, MapperKind, mapSchema } from "@graphql-tools/utils";
 
 export function transformDirective(directiveName) {
   return {
-    transformDirectiveTypeDefs: `directive @${directiveName}(widht: Int, height: Int) on FIELD`,
+    transformDirectiveTypeDefs: `directive @${directiveName}(width: Int, height: Int) on FIELD`,
     transformDirectiveTransformer: (schema) => {
       return mapSchema(schema, {
         [MapperKind.OBJECT_FIELD]: (fieldConfig) => {
@@ -11,29 +11,33 @@ export function transformDirective(directiveName) {
 
           fieldConfig.resolve = async function (source, args, context, info) {
             let result = await resolve(source, args, context, info);
-
             const fieldNode = info.fieldNodes[0];
-            const directive = fieldNode.directives?.find(directive => directive.name.value === directiveName);
-            if (directive && typeof result === 'string') {
-              return result.toUpperCase();
+            const directive = fieldNode.directives?.find(
+              (directive) => directive.name.value === directiveName
+            );
+            if (directive) {
+              const widthArg = directive.arguments?.find(
+                (arg) => arg.name.value === "width"
+              );
+              const widthValue = widthArg ? info.variableValues?.width : null;
+              const heightArg = directive.arguments?.find(
+                (arg) => arg.name.value === "height"
+              );
+              const heightValue = heightArg
+                ? info.variableValues?.height
+                : null;
+              if (heightValue || widthValue) {
+                const url = new URL(result);
+                if (widthValue !== undefined) {
+                  url.searchParams.set("w", widthValue);
+                }
+                if (heightValue !== undefined) {
+                  url.searchParams.set("h", widthValue);
+                }
+                return url;
+              }
+              return result;
             }
-
-            // if (directives && directives.length > 0) {
-            //   const directiveArgs = directives[0];
-            //   const { width, height } = directiveArgs;
-
-            //   if (typeof result === "string") {
-            //     const url = new URL(result);
-            //     if (width !== undefined) {
-            //       url.searchParams.set("w", width);
-            //     }
-            //     if (height !== undefined) {
-            //       url.searchParams.set("h", height);
-            //     }
-            //     result = url.toString();
-            //   }
-            // }
-
             return result;
           };
 
